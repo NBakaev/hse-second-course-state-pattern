0ab98af7abc90d7d4e48d184b3ebb1a5959682a7
Bakaev Nikita <ya@nbakaev.ru>
2014-12-06 16:33:43 +0300

    RC release + updating README.MD

--- StatePattern/StatePattern.cpp
+++ StatePattern/StatePattern.cpp
@@ -3,36 +3,54 @@
 
 using namespace std;
 
-class Machine{
-	class MachineActions *current;
 
-  public:
-    Machine();
-	void setCurrent(MachineActions *s) {
-        current = s;
-    }
+/*
+This is class which has property "current" which 'contain' (really just link) object which realise interface "IMachineActions"
+*/
+class Machine{
+	class IMachineActions *current;
+
+public:
+	Machine();
+
+	void setCurrent(IMachineActions *s) {
+		current = s;
+	}
 
 	int currentSpeed;
 
-	 string getName();
-	 int getMaxSpeed();
-	 int getCurrentSpeed();
+	string getName();
+	int getMaxSpeed();
+	int getCurrentSpeed();
+	bool faster();
+	bool slower();
 
 };
 
-
-class MachineActions{
+/*
+In normal languages it should be just interface but...
+*/
+class IMachineActions{
 public:
-	virtual void faster(Machine *m) = 0;
-	virtual void slower(Machine *m) = 0;
+	virtual bool faster(Machine *m) = 0;
+	virtual bool slower(Machine *m) = 0;
 	virtual string getName(Machine *m) = 0;
 	virtual int getMaxSpeed(Machine *m) = 0;
-
 };
 
 
 
+/*
+Default constructor - may be interesting in some cases
+*/
+Machine::Machine(){
+}
 
+
+
+/*
+Implementing methods of the object "Machine"
+*/
 string Machine::getName(){
 	return current->getName(this);
 }
@@ -45,107 +63,144 @@ int Machine::getCurrentSpeed(){
 	return currentSpeed;
 }
 
+bool Machine::faster(){
+	return current->faster(this) ;
+}
 
-class DefaultMachine : public MachineActions , public Machine{
-public:
+bool Machine::slower(){
+	return current->slower(this);
+}
 
-	virtual string getName(Machine *m){
-		return "DefaultMachine";
-	}
 
-	virtual void faster(Machine *m){
-	}
+/*
+Default car - can't ride and hasn't any actions like lada calina :)
+*/
 
-	virtual void slower(Machine *m) {
-	}
+class DefaultMachine : public IMachineActions, public Machine{
+	public:
 
-	virtual int getMaxSpeed(Machine *m){
-		return maxSpeed;
-	}
+		virtual string getName(Machine *m){
+			return name;
+		}
+
+		virtual bool faster(Machine *m){
+			return false;
+		}
+
+		virtual bool slower(Machine *m) {
+			return false;
+		}
+
+		virtual int getMaxSpeed(Machine *m){
+			return maxSpeed;
+		}
 
 	private:
 		int maxSpeed = 0;
+		string name = "DefaultMachine";
 };
 
 
-class CoolCabriolet :  public Machine,  public MachineActions{
-  public:
-	  CoolCabriolet() {
-        cout << "   You have choose CoolCabriolet ";
-    };
-	  ~CoolCabriolet()  {
-        cout << "   You have broke CoolCabriolet :( ";
-    };
+/*
+Realisation of CoolCabriolet; Implementing IMachineActions
+*/
+class CoolCabriolet : public IMachineActions{
+public:
+	CoolCabriolet() {
+		cout << "You have choosed CoolCabriolet";
+	};
+	~CoolCabriolet()  {
+		cout << "You broked CoolCabriolet :( ";
+	};
 
 
 	virtual string getName(Machine *m){
-		return "CoolCabriolet";
+		return name;
 	}
 
-	virtual void faster(Machine *m){
-		m->currentSpeed += 10;
+	virtual bool faster(Machine *m){
+		if (maxSpeed > m->getCurrentSpeed()){
+			m->currentSpeed += 10;
+			return true;
+		}
+
+		return false;
 	}
 
-	virtual void slower(Machine *m) {
-		m->currentSpeed -= 10;
+	virtual bool slower(Machine *m) {
+		if (m->getCurrentSpeed() - 10 >= 0){
+			m->currentSpeed -= 10;
+			return true;
+		}
+
+		return false;
 	}
 
 	virtual int getMaxSpeed(Machine *m){
 		return maxSpeed;
 	}
 
-	private:
-		int maxSpeed = 60;
+private:
+	int maxSpeed = 60;
+	string name = "CoolCabriolet";
 
 };
 
-class OldRussianCar : public MachineActions, public Machine
+
+/*
+Realisation of OldRussianCar; Implementing IMachineActions
+*/
+class OldRussianCar : public IMachineActions
 {
-  public:
-	  OldRussianCar()
-    {
-        cout << "   You have choose OldRussianCar ";
-    };
-	  ~OldRussianCar()
-    {
-        cout << "    You have broke OldRussianCar";
-    };
+	public:
+		OldRussianCar(){
+			cout << "   You have choose OldRussianCar ";
+		};
 
-	  virtual void faster(Machine *m){
-		  m->currentSpeed += 3;
-	  }
+		~OldRussianCar(){
+			cout << "    You have broke OldRussianCar";
+		};
 
-	  virtual void slower(Machine *m) {
-		  m->currentSpeed -= 5;
-	  }
+		virtual bool faster(Machine *m){
+			if (maxSpeed > m->getCurrentSpeed()){
+				m->currentSpeed += 5;
+				return true;
+			}
 
-	  virtual string getName(Machine *m){
-		  return "OldRussianCar";
-	  }
+			return false;
+		}
 
-	  virtual int getMaxSpeed(Machine *m){
-		  return maxSpeed;
-	  }
+		virtual bool slower(Machine *m) {
+			if (m->getCurrentSpeed() - 5 >= 0){
+				m->currentSpeed -= 5;
+				return true;
+			}
 
-	  
+			return false;
+		}
+
+		virtual string getName(Machine *m){
+			return name;
+		}
+
+		virtual int getMaxSpeed(Machine *m){
+			return maxSpeed;
+		}
+
 
 	private:
 		int maxSpeed = 20;
-
-
+		string name = "OldRussianCar";
 };
 
 
 
-
-Machine::Machine(){
-}
 
 
 
 void chooseCar(int car, Machine &fsm){
 	if (car == 1){
-		fsm.setCurrent(new CoolCabriolet() );
+		fsm.setCurrent(new CoolCabriolet());
 	}
 
 	if (car == 2){
@@ -154,83 +209,125 @@ void chooseCar(int car, Machine &fsm){
 
 	if (car == 3){
 		cout << "\n\n";
-		cout << "\n\n Your car is: \n";
+		cout << "\n\n Your car is: ";
 		cout << fsm.getName().c_str();
+		cout << "\n\n";
 		system("pause");
 		cout << "\n\n";
 	}
 
-	/*if (car == 4){
-		cout << "\n\n";
-		cout << "\n\n Your car is: \n";
-		cout << fs;
-		system("pause");
-		cout << "\n\n";
+	
+	if (car == 4){
+	cout << "\n\n";
+	cout << "\n\n Your car speed is : ";
+	cout << fsm.getCurrentSpeed();
+	cout << "\n\n";
+	system("pause");
+	cout << "\n\n";
 	}
 
 	if (car == 5){
-		cout << "\n\n";
-		cout << "\n\n Your car is: \n";
-		cout << fsm.getName().c_str();
-		system("pause");
-		cout << "\n\n";
-	}*/
+		if (fsm.faster()){
+			cout << "\n\n";
+			cout << "\n\n Your car speed is increased and now it is: ";
+			cout << fsm.getCurrentSpeed();
+			cout << "\n\n";
+			system("pause");
+			cout << "\n\n";
+		}
+		else
+		{
+			cout << "can't faster \n Sorry. bro :(";
+			cout << "\n\n";
+			system("pause");
+		}
+	}
+
+	if (car == 6){
+		if (fsm.slower()){
+			cout << "\n\n";
+			cout << "\n\n Your car speed is decreased and now it is: ";
+			cout << fsm.getCurrentSpeed();
+			cout << "\n\n";
+			system("pause");
+			cout << "\n\n";
+		}
+		else{
+			cout << "can't slower \n Sorry. bro :(";
+			cout << "\n\n";
+			system("pause");
+		}
+		
+	}
 
 
 
 }
 
 
+/*
+This is just demo that's why code may be not so beautiful
+*/
+
 int main(){
 
 	Machine fsm = *new Machine();
 	fsm.setCurrent(new DefaultMachine());
 
-	string defaultMachine = "DefaultMachine";
+	string defaultMachine = fsm.getName();
 
-  int num;
-  while (1)
-  {
-	  system("cls");
+	int num;
+	while (1)
+	{
+		system("cls");
 
-	  if (fsm.getName().c_str() == defaultMachine){
-		  cout << "You can press following key to choose car: ";
-		  cout << "\n ";
-		  cout << "1 - choose Cool Cabriolet";
-		  cout << "\n ";
-		  cout << "2 - choose old russian car";
-		  cout << "\n ";
-		  cin >> num;
-		  chooseCar(num, fsm);
+		if (fsm.getName().c_str() == defaultMachine){
+			cout << "You can press following key to choose car: ";
+			cout << "\n";
+			cout << " 1 - choose Cool Cabriolet \n";
+			cout << " 2 - choose old russian car \n";
 
-	  }
-	  else{
+			cin >> num;
+			chooseCar(num, fsm);
 
-		  if (fsm.getName().c_str() != defaultMachine){
-			  cout << "You have a car";
-			  cout << fsm.getName().c_str();
-			  cout << "\n ";
-			  cout << "\n ";
+		}
+		else{
 
-			  cout << "You can press following key to interact with car: ";
-			  cout << "\n ";
-			  cout << "3 - get name";
-			  cout << "\n ";
+			if (fsm.getName().c_str() != defaultMachine){
+				cout << "----------------------------------\n";
+				cout << "You have a car:  ";
+				cout << fsm.getName().c_str();
+				cout << "\nMax speed is:  " << fsm.getMaxSpeed();
+				cout << "\n\n\n";
+				cout << "----------------------------------\n";
 
-			  cout << "1 - choose Cool Cabriolet";
-			  cout << "\n ";
-			  cout << "2 - choose old russian car";
-			  cout << "\n ";
-			  cin >> num;
-			  cout << "\n ";
+				cout << "\n";
 
-			  chooseCar(num, fsm);
-		  }
+				cout << "You can press following key to interact with car: \n";
+				cout << " 3 - get name \n";
+				cout << " 4 - get current speed \n";
+				cout << " 5 - ride faster \n";
+				cout << " 6 - ride slower \n";
+				cout << "----------------------------------\n";
+
+				cout << "\n";
+
+				cout << "        Or you can choose another car\n";
+				cout << " 1 - choose Cool Cabriolet \n";
+				cout << " 2 - choose old russian car \n";
+				cout << "----------------------------------\n";
+
+				cout << "\n ";
+				cin >> num;
+				cout << "\n ";
+
+				chooseCar(num, fsm);
+			}
 
 
 
-	  }
+		}
 
 
-	  }
+	}
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,35 @@
-hse-second-course-state-pattern
+HSE 13PI Second course "State Pattern"
 ===============================
+Created by Bakaev Nikita
 
-# first versiona
+Sorry for my English
+
+## How does it work?
+
+We have class `Machine` which has field `current` which contains Class which implemented `IMachineActions` interfase.
+
+And we have two implementation of the `IMachineActions`:
+
+- **DefaultMachine** - You can't do anything with this car. You should choose another to continie
+- **CoolCabriolet** - It's a *good* car. It's for you, bro.
+- **OldRussianCar** - It's a *bad* car. It's for, bro.
+
+## How to use ???
+Create with public constructor and `setCurrent` which get an object implementing `IMachineActions`
+
+Depends of implementaion, you will have different max speed of the car, changes in the number of speed when you call the slow and fast.
+ 
+
+    Machine fsm = *new Machine();
+    fsm.setCurrent(new DefaultMachine());
+
+## IMachineActions
+You should override following methods to creat and implementation of the interface:
+
+	virtual bool faster ()
+	virtual bool slower()
+	virtual string getName()
+	virtual int getMaxSpeed()
+
+
+### That's all
